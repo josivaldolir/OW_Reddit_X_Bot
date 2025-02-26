e6fbd9c48f147f07e48f03122f9718a90b599a03
Jovi <josivaldolira54321@gmail.com>
2025-02-26 19:40:37 -0300

    Encrypted the data

--- oauth.py
+++ oauth.py
@@ -1,18 +1,18 @@
-import praw
+import praw, os
 
 reddit = praw.Reddit(
-    client_id="pQ99FMScB_rf2GlmvkJ0UQ",
-    client_secret="BiLfsWDS-MARG8ynfmg18sQ6fdfD8Q",
-    username="Loose-Chemist-4695",
-    password="Mamamia321",
-    user_agent="owxbot"
+    client_id=os.getenv('CLIENT_ID'),
+    client_secret=os.getenv('CLIENT_SECRET'),
+    username=os.getenv('USERNAME'),
+    password=os.getenv('PASSWORD'),
+    user_agent=os.getenv('USER_AGENT')
 )
 
-OAuth2_Client_ID = "RE1uSi1GVmdmb2VBLU5QSldubHk6MTpjaQ"
-OAuth2_Client_Secret = "sxPoUZzJFb5nUiXf_SBSV83k_aJ1EJy2YlN4XY6J0AGCUbJVFS"
+OAuth2_Client_ID = os.getenv('OAUTH2_CLIENT_ID')
+OAuth2_Client_Secret = os.getenv('OAUTH2_CLIENT_SECRET')
 
-api_key = "NnMMt1bgO1qLjEEiYZ31NZbpZ"
-api_secret = "5eemp35DxBopWxX7oPZCTj1eiHQHmo7JYfeDwGveqzBXZB1Ivn"
-bearer_token = r"AAAAAAAAAAAAAAAAAAAAAAfvzAEAAAAAU0j5Ee2GwYc1OD9%2FyoXw7Cnfhy8%3DM2WtuX8ylYWyGsZUZN9I0nw5P22BX5vPAOus9fHaYBBejRplp9"
-access_token = "1890211702186106880-jNb6ggmLmfNP89nqnDryFdK4olba2C"
-access_token_secret = "Sori7NPwZ8Wgt1KrJ78KJtAhxMoVl7fsoZZt2HgvSq2ey"
+api_key = os.getenv('CONSUMER_KEY')
+api_secret = os.getenv('CONSUMER_SECRET')
+bearer_token = os.getenv('BEARER_TOKEN')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
